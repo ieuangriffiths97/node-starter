e2db8b6a0432d46d2e6936e949765cca1e65ccb7
Ieuan <griff.ieuan@gmial.com>
2018-06-12 20:42:40 +0100

    messing around with some basics

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-var stuff = require("./stuff");
+var express = require('express');
+var fs = require('fs');
+var id = require('uniqid');
+var app = express();
 
-console.log(stuff.counter(['name1', 'name2', 'name3']));
 
-console.log(stuff.adder(5,6));
 
-console.log(stuff.adder(stuff.pi, 5));
+app.get('/', function (req, res) {
+  var string = '\nadded text';
+  res.send(string)
+  // get all reps
+  fs.appendFile('text.txt', string)
+})
 
+app.get('/read', function (req, res) {
+  const file = fs.readFileSync('text.txt')
+  res.send(file + ' and I am reading from it');
+  //res.json()
+  //res.sendFile('index.html')
+});
+
+app.get('/newGet/:rand/', function (req, res) {
+  res.send(req.params.rand)
+})
+var number = 0;
+
+app.post('/message/:message', function (req, res) {
+  var message = req.params.message;
+  number++;
+
+  res.send(message + " " + number);
+
+});
+
+app.get('/message/:id', function (req, res) {
+  var id = req.params.id
+  if (id != typeof number) {
+    res.send("ID has to  be a number")
+  }
+
+  const messages = fs.readFileSync('messages.txt')
+
+});
+
+
+app.put('/', function (req, res) {
+
+})
+
+app.delete('/delete', function (req, res) {
+  fs.unlinkSync('text.txt')
+  res.send("files deleted")
+})
+
+
+app.listen('9000', function () {
+  console.log('Server listening on port 9000')
+})
---
+++ refresher.js
@@ -0,0 +1,24 @@
+//string, numbers, null, boolean, undefined
+
+//object, functions, arrays
+
+var person = {
+  name: "Kenneth",
+  age: 42,
+  race: 'Asian',
+  height: 6.7,
+  personality: {
+    likability: 8,
+    sweetness: 10,
+  },
+  hobbies: ['dancing', 'singing', 'jumping']
+  
+
+}
+
+var myArr = [1, 'Sing', '1.8', [9, 0], { name: 'Lovely' }]
+
+console.log(person.name)
+console.log(person)
+console.log(person.personality.likability)
+console.log(person.addPersonality())
