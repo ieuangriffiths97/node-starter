bcd1113ce01986f17e7b4a997d8715b6bb585de5
Ieuan <griff.ieuan@gmial.com>
2018-06-13 19:43:48 +0100

    serving json

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ var fs = require('fs');
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
     res.writeHead(200,{'Content-Type': 'text/html'});
-    var readStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    readStream.pipe(res);
-    
+    var readStream = fs.createReadStream(__dirname + '/application/json', 'utf8');
+    var myObject = {
+        name: 'ieuan',
+        job: 'developer',
+        age: 21
+    };
+    res.end(JSON.stringify(myObject));    
 });
 
 server.listen(3000, '127.0.0.1');
