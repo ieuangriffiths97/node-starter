92d4848b0d39560ec7eb84189bcd27877ecd4566
Ieuan <griff.ieuan@gmial.com>
2018-06-13 19:20:01 +0100

    readable stream

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 
+var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+
+readStream.on('data', function(chunk){
+console.log('new chunck recieved');
+console.log(chunk);
+});
+
+/*
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
     res.writeHead(200,{'Content-Type': 'text/plain'});
@@ -9,4 +18,4 @@ var server = http.createServer(function(req,res){
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
-
+*/
