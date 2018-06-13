0d9b74be3b014ce5858987719d6e6da24102317e
Ieuan <griff.ieuan@gmial.com>
2018-06-13 19:32:10 +0100

    pipes

--- app.js
+++ app.js
@@ -1,25 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
-var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var writeStream = fs.createWriteStream(__dirname + '/writemeStream.txt');
 
-readStream.on('data', function(chunk){
-console.log('new chunck recieved:');
-console.log(chunk);
-
-writeStream.write(chunk);
-});
-
-
-/*
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
     res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('hello');
+    var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+    readStream.pipe(res);
     
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to port 3000');
-*/
+
