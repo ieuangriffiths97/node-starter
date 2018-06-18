8a2a97cacf2904ef481d84b47e3dffccbda2430c
Ieuan <griff.ieuan@gmial.com>
2018-06-18 23:49:42 +0100

    query strings

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs')
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact', function(req, res){
-    res.sendFile(__dirname + '/contact.html');
+    console.log(req.query);
+    res.render('contact', {qs: req.query});
 });
 
 app.get('/profile/:name', function(req, res){
