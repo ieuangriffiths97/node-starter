020c4c7f7932ad2f69b9e83c68b80cacf738bd7b
Ieuan <griff.ieuan@gmial.com>
2018-06-19 00:29:30 +0100

    post requests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.set('view engine', 'ejs')
 app.use('/assets', express.static('assets'));
@@ -9,11 +13,20 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/contact', function(req, res){
-    console.log(req.query);
+app.get('/contact', urlencodedParser, function(req, res){
     res.render('contact', {qs: req.query});
 });
 
+app.post('/contact', urlencodedParser, function(req, res){
+    console.log(req.body);
+    res.render('contact-success', {data: req.body});
+});
+/*
+app.post('/login', urlencodedParser, function (req, res) {
+    if (!req.body) return res.sendStatus(400)
+    res.send('welcome, ' + req.body.username)
+  });
+*/
 app.get('/profile/:name', function(req, res){
 
     var data = {
